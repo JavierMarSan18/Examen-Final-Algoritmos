d9c13ac23dcd24f8bdba0971c247ef2f50c968e5
Javier <https://github.com/JavierMarSan18>
2020-11-15 09:02:36 -0600

    first commit

---
+++ Agente 1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using Experimental.System.Messaging;
+
+namespace Agente_1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Declarar Variables
+            bool condition = true;
+            int time = 1000;
+            //Conexión a la cola
+            MessageQueue queueServer = new MessageQueue(".\\Private$\\ExamenFinal");
+
+            while(condition)
+            {
+
+                //Crear objeto
+                Data data = new Data();
+                //Asignar Valor aleatorio
+                data.filename = Path.GetRandomFileName();
+
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("             Generador de Nombres de Aleatorios            ");
+                Console.WriteLine("                   Desarrollado por JJMS                   ");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.Write($"Mensaje {data.filename}  Encolando");
+                wait(time);
+                sendData(queueServer, data);
+                //Mensaje de proceso exitos
+                Console.WriteLine($"El mensaje se ha encolado con éxito");
+                Thread.Sleep(time);
+                //Método para encolar el mensaje
+                Console.Clear();
+            }
+
+        }
+        static void wait(int time)
+        {
+              Thread.Sleep(time);
+              Console.Write(".");
+              Thread.Sleep(time);
+              Console.Write(".");
+              Thread.Sleep(time);
+              Console.Write(".\n\n");
+              Thread.Sleep(time);
+        }
+        static void sendData(MessageQueue queueServer, Data data)
+        {
+                //Convertir a Json
+                string jsonData = JsonConvert.SerializeObject(data);
+
+                //Empaquetar mensaje
+                Message msg = new Message();
+                msg.Body = jsonData;
+
+                //Encolar mensaje
+                queueServer.Send(msg);
+        }
+        internal class Data
+        {
+            public string filename{get;set;}
+        }
+    }
+}
---
+++ Agente 2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System.Threading;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Text;
+using Experimental.System.Messaging;
+
+namespace Agente_2
+{
+    class Program
+    {
+    	public static IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "ueYRXmsJTgdq76OJSzOqYIfqSchg3aIFdyjwdYMN",
+            BasePath = "https://examen-final-4dffa.firebaseio.com/"
+        };
+        static void Main(string[] args)
+        {
+        	//Declarar variables
+        	bool condition = true;
+        	int time = 1000;
+
+
+        	//Conexión a la cola
+            MessageQueue queueServer = new MessageQueue(".\\Private$\\ExamenFinal");
+
+            while(condition)
+           	{
+           		DateTime now = DateTime.UtcNow.AddHours(-6);
+           		string date = now.Day.ToString("00") + "-" + now.Month.ToString("00") + "-" + now.Year.ToString("0000") + " " + now.Hour.ToString("00") + ":" + now.Minute.ToString("00") + ":" + now.Second.ToString("00");
+           		Message jsonData = queueServer.Receive();
+	            jsonData.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+	            //string filename = jsonData.Body.ToString();
+	            Data msg = JsonConvert.DeserializeObject<Data>(jsonData.Body.ToString());
+	            string filename = msg.filename;
+
+	            Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("               Extracción y Subida de Cola                 ");
+                Console.WriteLine("                  Desarrollado por JJMS                    ");
+                Console.WriteLine("-----------------------------------------------------------");
+              
+
+                Console.WriteLine($"Mensaje {filename}  Encontrado");
+	            Console.Write("Subiendo a Base de Datos de Firebase");
+	          	wait(time);
+            	sendDB(date,msg);
+            	Console.WriteLine("El mensaje se ha enviado con éxito");
+
+            	Thread.Sleep(time);
+                Console.Clear();
+
+            }
+        }
+        static void wait(int time)
+        {
+        	  Thread.Sleep(time);
+	          Console.Write(".");
+	          Thread.Sleep(time);
+	          Console.Write(".");
+	          Thread.Sleep(time);
+	          Console.Write(".\n\n");
+           	  Thread.Sleep(time);
+        }
+        public static async void sendDB(string date, Data msg)
+        {
+
+        	IFirebaseClient client;
+            client = new FireSharp.FirebaseClient(config);
+            await client.UpdateTaskAsync("/atencion/"+date, msg);
+        }
+
+       	internal class Data
+        {
+            public string filename{get;set;}
+        }
+    }
+}
